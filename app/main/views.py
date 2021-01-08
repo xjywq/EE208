901ea04e566515323a98b002a10c283510590016
pixas <jiangshuyang@sjtu.edu.cn>
2021-01-08 19:05:19 +0800

    完整了result.html的pagination

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,5 @@
 from flask import render_template, session, redirect, url_for, request
-
+from flask_paginate import Pagination, get_page_parameter
 
 from . import main
 from .forms import SearchForm
@@ -31,7 +31,17 @@ def result():
         command = form.data["content"]
         # Todo search
         search_res = SportItem.query.filter_by(id=int(command)).first()
-        return render_template('result.html', res=search_res)
+        # 我们认为search_res是一个数组，首先统计一下，然后下载pagination
+        found = len(search_res)
+        page = request.args.get(get_page_parameter(), type=int, default=1)
+        
+        per_page = int(request.args.get('per_page', default = 40)) # 这样可以整除
+        # 要传进template的代码
+        res = [single for single in search_res[(page - 1) * per_page, page * per_page]]
+        res = [[each for each in res[i * 4, i * 4 + 4]] for i in range(per_page / 4)]
+        
+        pagination = Pagination(found = found, page = page, search = True, total = found, per_page = per_page)
+        return render_template('result.html', res=res, keyword = form.data, pagenation = pagination)
     else:
         print(form.errors, "错误信息")
     return render_template("index.html", form=form)
